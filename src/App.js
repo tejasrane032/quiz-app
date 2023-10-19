b4278b0abf9166168a329fdbab41cdbd16713e1b
tejasrane032 <109644492+tejasrane032@users.noreply.github.com>
2023-10-19 09:10:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { nanoid } from 'nanoid' ;
+import Confetti from 'react-confetti' ;
+import { useState, useEffect } from 'react' ;
+import { useWindowSize } from 'react-use' ;
+import Intro from './components/Intro' ;
+import Spinner from './shared/Spinner' ;
+import Questions from './components/Questions' ;
+
+
+function App() {
+    const {width, height} = useWindowSize() ;
+    const [start, setStart] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [questions, setQuestions] = useState([]);
+    const [checkedQuestion, setCheckedQuestion] = useState(false);
+    const [score, setScore] = useState(0);
+    const [playAgain, setPlayAgain] = useState(1);
+    const [showConfetti, setShowConfetti] = useState(false);
+    
+
+    // Handle clicking start the quiz button, if so render the questions component
+    function startQuiz() {
+        setStart(true);
+    }
+
+    // Any answer get clicked will stored its value to the selectedAnswer's question object
+  
+    // Handeling the click check answers as well as play again
+    function checkAnswers() {
+        // Handle playing again
+        if(checkedQuestion) {
+            setQuestions([]);
+            setIsLoading(true);
+            setCheckedQuestion(false);
+            setPlayAgain( prevSetPlayAgain => prevSetPlayAgain + 1 );
+            return ;
+
+        } else {
+            // Check if all questions are checked, if so
+            setCheckedQuestion( () => {
+                return questions.every( question => question.checked );
+            });
+            // Then let me know how many answers are correct
+            setScore( () => {
+                return questions.filter( question => question.correctAnswer === question.selectedAnswer).length;
+            });
+
+            // Display the confetti
+            setShowConfetti(true);
+            
+            // Stop displaying the confetti
+            setTimeout( () => setShowConfetti(false), 5000);
+        }
+    }
+
+    // Fetching the data from the database and stop displaying the Spinner component 
+    
+    // Whenever the user click play again fetch a new data from the database again
+    useEffect( () => {
+        fetchQuestions();
+
+    }, [playAgain]);
+    
+    
+    return (
+        <div className='container'>
+            { (checkedQuestion && showConfetti) && <Confetti width={ width } height={ height }  /> }
+            {
+                !start 
+                ? <Intro 
+                    description='Dive into the world of IT with our interactive quiz and test your knowledge!'
+                    start={ start } 
+                    startQuiz={ startQuiz } 
+                />
+                
+                : isLoading ? <Spinner />
+                : <Questions 
+                    questions={ questions } 
+                    handleSelectingAnswers={ selectingAnswers } 
+                    checkedQuestion={ checkedQuestion } 
+                    handleCheckAnswers={ checkAnswers } 
+                    score={ score }
+                />
+            }
+        </div>
+    );
+}
+
+
+export default App ;
---
+++ src/components/Questions.jsx
@@ -0,0 +1,29 @@
+import Question from './Question' ;
+
+
+function Questions({ questions, handleSelectingAnswers, checkedQuestion, handleCheckAnswers, score }) {
+    // To avoid duplication of the id value and htmlFor, cause sometimes when I have
+    // Two questions they both their answers has to be true or false 
+    // Letting the id hold the value of the answer whether true or false, it's gonna let you only select one
+    let increment = 0;
+    
+
+    return (
+        <section className='questions-container'>
+            { questionsElement }
+            
+            <div className='action'>
+                { checkedQuestion &&  <p className='score'>You scored { score }/5 correct answers</p> }
+                <button
+                    className='check-answers-button'
+                    onClick={ handleCheckAnswers }
+                >
+                    { !checkedQuestion ? 'Check answers' : 'Play again' }
+                </button>
+            </div>
+        </section>
+    );
+}
+
+
+export default Questions ;
